1cada2660d34c4e42dd5d3145364099b927c7972
Intern User <Intern@DHK-HO-NB1021B.ebl.bd>
2022-03-23 17:25:04 +0600

    details added

--- src/components/TipsDetails.js
+++ src/components/TipsDetails.js
@@ -35,15 +35,20 @@ const TipsDetails = () => {
             <div  className="row d-flex align-items-center justify-content-center middle">
                 <div className="col-md-6 col-12">
                     <div className="mb-4 ">
-                        <img className="img-fluid w-75 h-50 margin  rounded" src={detail?.img} alt="" />
+                        <img className="img-fluid w-75 h-50 margin rounded-3" src={detail?.img} alt="" />
                     </div>
                 </div>
                 <div className="col-md-6 col-12">
                     {/* <h1>{detail?.id} </h1> */}
-                    <h4 className='text-primary text-center'> {detail?.name} </h4>
+                    <h4 className='text-primary text-center'> <b>{detail?.name}</b> </h4>
                     <hr className="w-75 mx-auto" />
                     
                     <h6 className="m-4"> {detail?.description} </h6>
+                   
+                    <h6 className="m-4"> {detail?.details} </h6>
+                    <h6 className="m-4"> {detail?.detail} </h6>
+                    <h6 className="m-4"> {detail?.last} </h6>
+
 
                 </div>
             </div>
